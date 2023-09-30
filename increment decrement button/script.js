3cadbbbb15ace6e13927094082e82969f1f2b943
tusharrr-31 <singhtushar4411@gmail.com>
2023-09-30 17:29:33 +0530

    +10 -10

--- increment decrement button/script.js
+++ increment decrement button/script.js
@@ -16,3 +16,19 @@ value = value-1;
 // set it into ui
 countvalue.innerText = value;
 }
+const addten =() =>{
+//    get the value from ui
+let value = parseInt(countvalue.innerText);
+// update the value
+value = value+10;
+// set it into ui
+countvalue.innerText = value;
+}
+const subten =() =>{
+//    get the value from ui
+let value = parseInt(countvalue.innerText);
+// update the value
+value = value-10;
+// set it into ui
+countvalue.innerText = value;
+}

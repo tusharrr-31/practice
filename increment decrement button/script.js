16fb5a87e225333c25f18bd5a74c1c27fe6a3700
tusharrr-31 <singhtushar4411@gmail.com>
2023-09-30 17:15:37 +0530

    home page

--- increment decrement button/script.js
+++ increment decrement button/script.js
@@ -2,7 +2,7 @@ const countvalue = document.querySelector('#counter')
 
 const increment =() =>{
 //    get the value from ui
-const value = parseInt(countvalue.innerText);
+let value = parseInt(countvalue.innerText);
 // update the value
 value = value+1;
 // set it into ui
@@ -10,7 +10,7 @@ countvalue.innerText = value;
 }
 const decrement =() =>{
 //    get the value from ui
-const value = parseInt(countvalue.innerText);
+let value = parseInt(countvalue.innerText);
 // update the value
 value = value-1;
 // set it into ui
